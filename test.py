c82f06f88e332d6e0e021f8d4ec30490ccfc1a11
Rachael Birky <rbirky1@jhu.edu>
2017-04-09 07:53:16 -0400

    Trying 0.0.0.0 with heroku open

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     # BUILD DATABASE
     engine = create_engine('sqlite:///tutorial.db')
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
     # app.run()
 
 
